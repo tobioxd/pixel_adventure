b755be858369285c5690624dd51c1ed45d2c9c94
Nhat Minh <minhnhat.kd.hungyen@gmail.com>
2024-09-10 15:47:40 +0700

    Adding Player to map

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:pixel_adventure/actors/player.dart';
 
 class Level extends World{
 
@@ -10,9 +11,26 @@ class Level extends World{
   @override
   FutureOr<void> onLoad() async {
 
-    level = await TiledComponent.load('Level-01.tmx', Vector2.all(16));
+    level = await TiledComponent.load('Level-02.tmx', Vector2.all(16));
 
     add(level);
+
+    final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+
+    for(final spawnPoint in spawnPointsLayer!.objects){
+
+      switch(spawnPoint.class_){
+        case 'Player':
+          final player = Player(
+            character: 'Mask Dude',
+            position: Vector2(spawnPoint.x , spawnPoint.y),
+          );
+          add(player);
+          break;
+        default:
+      }
+
+    }
 
     return super.onLoad();
 
