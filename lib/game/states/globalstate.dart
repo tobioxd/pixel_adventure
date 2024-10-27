787e84297b1d99056ce57fa231780c6d9e03c241
little-chloe <laptrinhcungtony@gmail.com>
2024-10-27 15:33:31 +0700

    add select player screen

--- lib/game/states/globalstate.dart
+++ lib/game/states/globalstate.dart
@@ -2,6 +2,7 @@ class GlobalState {
   static final GlobalState _instance = GlobalState._internal();
   int numberFruits = 0;
   int life = 0;
+  String playerName = '';
 
   factory GlobalState() {
     return _instance;
